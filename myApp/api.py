98944692de4c8e12103ecf812ddedee1ce21633b
moahmed hassan <moahmedegyptair@gmail.com>
2018-03-12 10:24:07 +0200

    Fix: URL API

--- myApp/api.py
+++ myApp/api.py
@@ -1,8 +1,11 @@
-from myApp import app, post_store
-from flask.json import jsonify
+from myApp import app, post_store, models
+from flask import jsonify, abort, request
 
 
 @app.route("/api/topic/all")
-def topic_api_all():
+def api_topic_all():
     posts = [post.__dict__() for post in post_store.get_all()]
     return jsonify(posts)
+
+
+
